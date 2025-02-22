c0d957f10a306244c6c92a9cbad0613734aea51c
ZoomL-Olymp <nnahalenok@gmail.com>
2025-02-22 16:46:41 +0300

    feat: new assets (dark and light empty tiles), added grid and tile layout

--- script.js
+++ script.js
@@ -40,6 +40,14 @@ window.onload = function() {
         assets.mascot2 = new Image();
         assets.mascot2.src = 'assets/mascot2.png';
         assets.mascot2.onload = imageLoaded;
+
+        assets.tile_dark = new Image();
+        assets.tile_dark.src = 'assets/tile_dark.png';
+        assets.tile_dark.onload = imageLoaded;
+
+        assets.tile_light = new Image();
+        assets.tile_light.src = 'assets/tile_light.png';
+        assets.tile_light.onload = imageLoaded;
     }
 
     loadAssets(function() {
@@ -48,8 +56,25 @@ window.onload = function() {
     });
 
     function drawGrid() {
-        // placeholder square
-        ctx.fillStyle = 'red';
-        ctx.fillRect(10, 10, 50, 50);
-    }
+        const gridWidth = 4;
+        const gridHeight = 5;
+        const tileSize = 90; // size in px
+        const padding = 5; // padding in px
+
+        for (let row = 0; row < gridHeight; row++) {
+            for (let col = 0; col < gridWidth; col++) {
+                tileImage = new Image();
+                if ((row + col) % 2 == 0) {
+                    tileImage = assets[`tile_light`];
+                } else {
+                    tileImage = assets[`tile_dark`];
+                }
+
+                const x = col * (tileSize + padding);
+                const y = row * (tileSize + padding);
+
+                ctx.drawImage(tileImage, x, y, tileSize, tileSize);
+            }
+        }
+}
 };
